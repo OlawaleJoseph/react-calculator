bfe2d6cf43086dfce3f0241447677e96cdda3158
OlawaleJoseph <adedekojoseph@gmail.com>
2020-11-25 22:57:12 +0100

    create and connect click event handlers with buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,83 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, Component, Fragment } from 'react';
 import ButtonPannel from './ButtonPanel';
 import Display from './Display';
 import Button from './Button';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-function App() {
-  const buttons = [
-    'AC', 'X²', '%', '/',
-    '9', '8', '7', '*',
-    '6', '5', '4', '+',
-    '3', '2', '1', '-',
-    '0', '.', '+/-', '=',
-  ];
+const buttons = [
+  'AC', 'X²', '%', '/',
+  '9', '8', '7', '*',
+  '6', '5', '4', '+',
+  '3', '2', '1', '-',
+  '0', '.', '+/-', '=',
+];
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      prev: '0',
+      next: '0',
+      operator: null,
+    };
+  }
 
-  return (
-    <>
-      <div className="App">
-        <div className="App__calculator">
-          <Display />
-          <ButtonPannel>
-            {buttons.map(button => {
-              const operators = ['+', '-', '*', '/', '%', 'X²'];
-              const isOperator = operators.includes(button);
-              const isAcBtn = button === 'AC';
-              return (
-                <Button
-                  key={button}
-                  name={button}
-                  operator={isOperator}
-                  acBtn={isAcBtn}
-                />
-              );
-            })}
-          </ButtonPannel>
+  handledisplay = (prev, next, tot, op) => {
+    let ans;
+    if (tot) {
+      ans = tot;
+    } else if (!op) {
+      ans = prev;
+    } else {
+      ans = next;
+    }
+    return ans;
+  };
+
+  handleClick = e => {
+    const {
+      total, next, operator, prev,
+    } = this.state;
+    const res = calculate({
+      total, next, operator, prev,
+    }, e.target.value);
+
+    this.setState({
+      ...res,
+    });
+  };
+
+  render() {
+    const {
+      prev, next, total, operator,
+    } = this.state;
+    return (
+      <>
+        <div className="App">
+          <div className="App__calculator">
+            <Display current={this.handledisplay(prev, next, total, operator)} />
+            <ButtonPannel>
+              {buttons.map(button => {
+                const operators = ['+', '-', '*', '/', '%', 'X²'];
+                const isOperator = operators.includes(button);
+                const isAcBtn = button === 'AC';
+                return (
+                  <Button
+                    onClick={this.handleClick}
+                    key={button}
+                    name={button}
+                    operator={isOperator}
+                    acBtn={isAcBtn}
+                  />
+                );
+              })}
+            </ButtonPannel>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Button({
-  name, operator, acBtn,
+  name, operator, acBtn, onClick,
 }) {
   return (
     <button
       className={`btn ${operator ? 'operatorBtn' : ''} ${acBtn ? 'acBtn' : ''}`}
       type="button"
       value={name}
+      onClick={onClick}
     >
       {name}
     </button>
@@ -24,4 +25,5 @@ Button.propTypes = {
   name: PropTypes.string.isRequired,
   operator: PropTypes.bool,
   acBtn: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
 };
