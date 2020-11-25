05527b1c8f1e734db27b117dd10a44fd8cc01f0f
OlawaleJoseph <adedekojoseph@gmail.com>
2020-11-25 08:33:08 +0100

    refactor button panel

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,22 @@ import Display from './Display';
 import Button from './Button';
 
 function App() {
+  const buttons = [
+    'AC', 'X²', '%', '/',
+    '9', '8', '7', '*',
+    '6', '5', '4', '+',
+    '3', '2', '1', '-',
+    '0', '.', '+/-', '=',
+  ];
+
   return (
     <div className="App">
       <div className="App__calculator">
         <Display />
         <ButtonPannel>
-          <Button name="AC" />
-          <Button name="X²" />
-          <Button name="%" />
-          <Button name="/" />
-          <Button name="9" />
-          <Button name="8" />
-          <Button name="7" />
-          <Button name="*" />
-          <Button name="6" />
-          <Button name="5" />
-          <Button name="4" />
-          <Button name="+" />
-          <Button name="3" />
-          <Button name="2" />
-          <Button name="1" />
-          <Button name="-" />
-          <Button name="0" />
-          <Button name="." />
-          <Button name="+/-" />
-          <Button name="=" />
+          {buttons.map(button => (
+            <Button key={button} name={button} />
+          ))}
         </ButtonPannel>
       </div>
     </div>
