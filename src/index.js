21ef552efe53a33d333a9fe77d752b9cb139de9e
OlawaleJoseph <adedekojoseph@gmail.com>
2020-11-29 19:11:14 +0100

    create home component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <Route exact to="/calculator" component={App} />
-    </React.StrictMode>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
