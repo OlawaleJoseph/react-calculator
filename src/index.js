f13ba644393b89665e8e2f41c6082e8bf2a755a1
OlawaleJoseph <adedekojoseph@gmail.com>
2020-11-29 18:38:41 +0100

    install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Route exact to="/calculator" component={App} />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root'),
 );
